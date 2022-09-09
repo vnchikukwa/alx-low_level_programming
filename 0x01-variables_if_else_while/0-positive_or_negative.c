dfcdcb275c421292e19effaff4c25982f9578556
vnchikukwa <vnchikukwa@gmail.com>
2022-09-09 14:53:39 -0700

    My task results

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  *
  *  * main - Prints a random number and states whether
@@ -9,7 +10,7 @@
  *
  *    *
  *
- *     * Return: Always 0.
+ *     * Return: Always 0
  *
  *      */
 int main(void)
