e8b3644ede0824bf2bc815829386f9b8025a7d85
vnchikukwa <vnchikukwa@gmail.com>
2022-09-09 15:43:17 -0700

    My task results

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
- *
- *  * main - Prints the alphabet in lowercase, except for q and e.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+* main - Prints the alphabet in lowercase, except for q and e.
+* 
+* Return: Always 0
+*/
 int main(void)
 {
 	char letter;
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 /**
- *
- *  * main - Prints all single digit numbers of base 10 starting from 0.
- *
- *   *
- *
- *    * Return: Always 0.
- *
- *     */
+* main - Prints all single digit numbers of base 10 starting from 0.
+*  
+* Return: Always 0
+*/
 int main(void)
 {
 	int num;
+
 	for (num = 0; num < 10; num++)
 		printf("%d", num);
 	printf("\n");
