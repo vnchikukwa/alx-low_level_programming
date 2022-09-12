99a56826da28112cc09cf0e39f260951b53a1cbd
vnchikukwa <vnchikukwa@gmail.com>
2022-09-12 12:05:43 -0700

    My task result

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - Prints the alphabet in lowercase, except for q and e
+*main - Prints the alphabet in lowercase, except for q and e
 * 
 * Return: Always 0
 */
@@ -9,6 +9,7 @@ int main(void)
 	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
+
 		if (letter != 'e' && letter != 'q')
 			putchar(letter);
 	}
