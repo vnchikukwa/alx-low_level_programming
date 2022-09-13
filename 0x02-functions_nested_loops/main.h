3729aea73c0fc2c89f00ab8ea331479c3218196d
vnchikukwa <vnchikukwa@gmail.com>
2022-09-13 15:19:47 -0700

    My task result

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -14,4 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-#endif / MAIN_H /
+
+#endif
