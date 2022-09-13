51051607b35f5c9ba289eac106e52f97c53e1f17
vnchikukwa <vnchikukwa@gmail.com>
2022-09-13 14:29:06 -0700

    Honor

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
-* main - check the code
-* Return: Always 0
-*/
-int main(void)
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
 {
-	    print_alphabet();
-	        return (0);
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	putchar('\n');
 }
