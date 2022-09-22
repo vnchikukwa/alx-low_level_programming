6754e3502b41254b4cbbd8fff3f6725d3aae1a3d
vnchikukwa <vnchikukwa@gmail.com>
2022-09-22 03:31:18 -0700

    Advanced

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
+
 /**
  * rot13 - Encodes a string using rot13
  * @str: The string to be encoded
  *
  * Return: A pointer to the encoded string
  */
+
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
+
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
 			     'M', 'N', 'O', 'P', 'Q', 'R',
