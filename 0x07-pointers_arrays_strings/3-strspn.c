98ba9427c46a81dc82018a9b3a12f672c8bc27f7
vnchikukwa <vnchikukwa@gmail.com>
2022-09-26 14:41:17 -0700

    Task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,40 +1,35 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
- **/
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
+ */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	int count = 0;
-	char *str1, *str2;
+	unsigned int bytes = 0;
+	int index;
 
-	str1 = s;
-	str2 = accept;
-
-	i = 0;
-	while (str1[i] != '\0') /*Declaring WHILE *s */
+	while (*s)
 	{
-		j = 0;
-		while (str2[j] != '\0') /*Declaring WHILE *accept*/
+		for (index = 0; accept[index]; index++)
 		{
-		if (str2[j] == str1[i]) /*Evaluate condition*/
-		{
-			count++; /*count number*/
-			break;
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+
 		}
 
-		j++;    /*add j+1*/
-
-	if (s[i] != accept[j]) /*If aren't equals*/
-	{
-		break;
+		s++;
 	}
-	i++; /*add x+1*/
-}
 
-return (count); /*return the value of count*/
+	return (bytes);
 }
